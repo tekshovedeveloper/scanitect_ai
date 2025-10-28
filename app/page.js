72f3d08e73906c85222cea7eaf9f5d51c6bf1b8b
M wayl <tekshovedeveloper@gmail.com>
2025-10-28 22:03:22 +0500

    hide video

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
       <div className="content">
         <HomeBanner />
         <AboutUs /> 
-        <GuideVideoSection />
+        {/* <GuideVideoSection /> */}
         <PowerfulIntelligent />
          <SmartAiTravel />
        <SmartAIBuiltSection />
