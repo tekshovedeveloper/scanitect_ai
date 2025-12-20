52bd071dbe6a43c751611912c1eafc9d11d4b709
M wayl <tekshovedeveloper@gmail.com>
2025-12-20 20:10:19 +0500

    change favicon

--- app/layout.js
+++ app/layout.js
@@ -29,9 +29,9 @@ export const metadata = {
     icon: [
       // { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       // { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
-      { url: '/favicon.ico' }
+      { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
+        { url: '/favicon-512x512' }
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
