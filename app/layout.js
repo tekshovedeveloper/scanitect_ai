b22c8f78d7d31b59cd4ec6f97fae18ec6a1a7905
M wayl <tekshovedeveloper@gmail.com>
2025-12-31 15:13:44 +0500

    resolve bug

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,11 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "../redux/providers";
 import { stores } from "../redux/store";
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation';
-import Sidebar from './components/Sidebar';
-import Header from './components/Header';
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 import Head from "next/head";
- 
-
 
 // export const metadata = {
 //   title: "ScanitectAi",
@@ -19,7 +17,7 @@ import Head from "next/head";
 //   alternates: {
 //     canonical: 'https://scanitectai.com/',
 //   },
- 
+
 // };
 
 export const metadata = {
@@ -27,48 +25,39 @@ export const metadata = {
   description: "",
   icons: {
     icon: [
-      // { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      // { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
-      { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
-        { url: '/favicon-512x512' }
+      { url: "/favicon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
-    ]
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
   },
   alternates: {
-    canonical: 'https://scanitectai.com/',
+    canonical: "https://scanitectai.com/",
   },
 };
 
-
-
 export const viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
-  viewportFit: 'cover',
+  viewportFit: "cover",
 };
 
 export default function RootLayout({ children }) {
   const cookieStore = cookies();
-  const isLoggedIn = cookieStore.get('access_token')?.value ? true : false;
+  const isLoggedIn = cookieStore.get("access_token")?.value ? true : false;
 
   return (
     <html lang="en">
-     
       <body>
         <Providers>
           <main className="min-h-screen">
-            <div className='flex flex-wrap'>
+            <div className="flex flex-wrap">
               {/* <div className='w-1/5 h-dvh bg-slate-700'>
                 <Sidebar />
               </div> */}
               <div className="w-full">
                 <Header isLoggedIn={isLoggedIn} />
-                <div className="w-full">
-                  {children}
-                </div>
+                <div className="w-full">{children}</div>
               </div>
             </div>
           </main>
--- app/page.js
+++ app/page.js
@@ -22,20 +22,18 @@ export const metadata = {
   description: "",
   icons: {
     icon: [
-      // { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      // { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
-      { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
-      { url: '/favicon-512x512.png' }
+      { url: "/favicon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [
-      { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' }
-    ]
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
   },
   alternates: {
-    canonical: 'https://scanitectai.com/',
+    canonical: "https://scanitectai.com/",
   },
 };
+
+
 
 export default function Home() {
   const cookieStore = cookies();
